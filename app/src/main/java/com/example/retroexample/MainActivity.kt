a996b9b247a09c0b35f239034d4dc64940cfe229
Sanchit Monga <sanchitmonga22@gmail.com>
2022-07-04 13:52:26 -0400

    - Updated the layout of the main activity. - Removed unused code.

--- app/src/main/java/com/example/retroexample/MainActivity.kt
+++ app/src/main/java/com/example/retroexample/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.retroexample.databinding.MainActivityBinding
 
@@ -14,18 +13,16 @@ class MainActivity : AppCompatActivity() {
     private val listItems = ArrayList<String>()
     private lateinit var adapter: ArrayAdapter<String>
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mViewModel = ViewModelProvider(this)[DummyDataVM::class.java]
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-        mBinding.quotesListView.adapter = adapter
-        val observer: Observer<List<String>> = Observer<List<String>> {
+        mViewModel.getQuotesRepository().observe(this) {
             updateList(it)
         }
-        mViewModel.getQuotesRepository().observe(this, observer)
+        mBinding.quotesListView.adapter = adapter
     }
 
     private fun updateList(loadedItems: List<String>) {
@@ -33,37 +30,4 @@ class MainActivity : AppCompatActivity() {
         listItems.addAll(loadedItems)
         adapter.notifyDataSetChanged()
     }
-
-
-//    override fun onResume() {
-//        super.onResume()
-//        var listItems: ArrayList<String>
-//        mViewModel.getQuotesRepository().let {
-//            listItems = mViewModel.getQuotesRepository().value!!
-//        }
-//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-//    }
 }
-
-/**
- * {
-"count": 20,
-"totalCount": 1885,
-"page": 1,
-"totalPages": 95,
-"lastItemIndex": 20,
-"results": [
-{
-"tags": [
-"famous-quotes",
-"inspirational"
-],
-"_id": "QdK00IhCNX",
-"author": "Larry Page",
-"content": "If you're changing the world, you're working on important things. You're excited to get up in the morning.",
-"authorSlug": "larry-page",
-"length": 106,
-"dateAdded": "2021-06-18",
-"dateModified": "2021-06-18"
-},
- */
